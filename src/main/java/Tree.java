868d2123be280a4cb4aff7c54da3a28775d64b74
Michael Wang <a0416517630@icloud.com>
2025-12-10 16:46:23 +1100

    implemented ls-tree

--- src/main/java/Tree.java
+++ src/main/java/Tree.java
@@ -1,6 +1,10 @@
+import java.io.ByteArrayOutputStream;
+import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Tree {
     public static void runLsTree(String hash) throws Exception {
@@ -13,8 +17,33 @@ public class Tree {
         // decompress tree object file
         byte[] compressedData = Files.readAllBytes(objectPath);
         byte[] decompressedData = Utils.decompressZlib(compressedData);
-        for (int i = 0; i < decompressedData.length; i++) {
-            System.out.println(decompressedData[i]);
+        List<String> splitData = byteToString(decompressedData);
+
+        for (int i = 1; i < splitData.size()-1; i++) {
+            String curr = splitData.get(i);
+            String[] parts = curr.split(" ");
+            String name = parts[parts.length-1];
+            System.out.println(name);
         }
+    }
+
+    public static List<String> byteToString(byte[] data) {
+        List<String> result = new ArrayList<>();
+        ByteArrayOutputStream buffer = new ByteArrayOutputStream();
+
+        for (byte b: data) {
+            if (b == 0) {
+                result.add(buffer.toString(StandardCharsets.UTF_8));
+                buffer.reset();
+            } else {
+                buffer.write(b);
+            }
+        }
+
+        if (buffer.size() > 0) {
+            result.add(buffer.toString(StandardCharsets.UTF_8));
+        }
+
+        return result;
     }
 }
